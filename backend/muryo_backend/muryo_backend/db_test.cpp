ed9a1862d8b4d548bcb0352fb5cdf98ece6d4e0f
Cheria <15796172+cheriaashley@user.noreply.gitee.com>
2026-04-14 16:26:13 +0800

    数据库结构修改以及后端实现方式改变（或许成功版）

---
+++ backend/muryo_backend/muryo_backend/db_test.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <mysql.h>
+
+int main() {
+    MYSQL* conn = mysql_init(nullptr);
+    if (conn == nullptr) {
+        std::cout << "mysql_init failed" << std::endl;
+        return 1;
+    }
+
+    if (mysql_real_connect(
+        conn,
+        "127.0.0.1",
+        "muryo_user",
+        "muryo123",
+        "muryo",
+        3306,
+        nullptr,
+        0
+    ) == nullptr) {
+        std::cout << "connect failed: " << mysql_error(conn) << std::endl;
+        mysql_close(conn);
+        return 1;
+    }
+
+    if (mysql_query(conn, "SELECT 1")) {
+        std::cout << "query failed: " << mysql_error(conn) << std::endl;
+        mysql_close(conn);
+        return 1;
+    }
+
+    MYSQL_RES* res = mysql_store_result(conn);
+    if (res == nullptr) {
+        std::cout << "store result failed: " << mysql_error(conn) << std::endl;
+        mysql_close(conn);
+        return 1;
+    }
+
+    MYSQL_ROW row = mysql_fetch_row(res);
+    if (row != nullptr) {
+        std::cout << "OK: " << row[0] << std::endl;
+    }
+    else {
+        std::cout << "no result" << std::endl;
+    }
+
+    mysql_free_result(res);
+    mysql_close(conn);
+
+    return 0;
+}
---
+++ backend/muryo_backend/muryo_backend/muryo_backend.cpp
@@ -0,0 +1,19 @@
+﻿// muryo_backend.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+/*
+#include <iostream>
+#include "httplib.h"
+
+int main() {
+    httplib::Server svr;
+
+    svr.Get("/test", [](const httplib::Request&, httplib::Response& res) {
+        res.set_content("{\"msg\":\"MURYO backend running\"}", "application/json");
+        });
+
+    std::cout << "Server started at " << std::endl;
+
+    svr.listen("127.0.0.1", 8080);
+
+    return 0;
+}*/
